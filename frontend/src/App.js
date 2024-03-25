a2fa098408b4493acca31365500b772fda733215
theophilusnuer <tphilus003@gmail.com>
2024-03-25 02:41:20 +0000

    setting up the dashboard interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
 import { Router, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Dashboard from './pages/dashboard';
 
 const router = createBrowserRouter(
   [
-    {path:'/', element: <Home/>}
+    // {path:'/', element: <Home/>}
+    {path:'/dashboard', element: <Dashboard/>}
+
   ]
 )
 
 function App() {
   return (
    <>
-   <Navbar/>
+   {/* <Navbar/> */}
    <RouterProvider router={router}/>
     </>
   );
