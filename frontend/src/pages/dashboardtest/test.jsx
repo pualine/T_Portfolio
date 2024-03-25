4726ac78f02d17df807d0e02659288a529fa9134
theophilusnuer <tphilus003@gmail.com>
2024-03-25 18:36:15 +0000

    admin dashboard

---
+++ frontend/src/pages/dashboardtest/test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import DbNavbar from '../../components/dashboardcomponents/db_navbar';
+
+const AdminDashboard = () => {
+  return (
+    <DbNavbar/>
+  );
+}
+
+export default AdminDashboard;
